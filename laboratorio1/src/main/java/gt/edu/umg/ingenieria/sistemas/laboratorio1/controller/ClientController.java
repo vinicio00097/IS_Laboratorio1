855608297b045995bbea64cf2573c8d6a46c5046
abarillas <abarillas@nbwaudev04>
2019-04-13 13:26:49 -0600

    Added service helper.

---
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/controller/ClientController.java
@@ -0,0 +1,15 @@
+package gt.edu.umg.ingenieria.sistemas.laboratorio1.controller;
+
+import gt.edu.umg.ingenieria.sistemas.laboratorio1.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Josué Barillas (jbarillas)
+ */
+public class ClientController {
+
+    @Autowired
+    private ReportService reportService;
+    
+}
